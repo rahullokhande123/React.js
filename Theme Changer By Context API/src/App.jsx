32d18d7839cde1b36071e6e1e4b7cfc4340d0e79
Rahul Lokhande <rlokhande06244@gmail.com>
2024-08-05 08:37:15 +0530

    Theme Changer

--- Theme Changer By Context API/src/App.jsx
+++ Theme Changer By Context API/src/App.jsx
@@ -1,10 +1,11 @@
 
-import { useState } from "react";
 
+import { useContext, useState } from "react";
+import { mycolorContext } from "./ColorContext";
 import { Container } from "react-bootstrap";
 
 const App=()=>{
- 
+  const {color,setColor}=useContext(mycolorContext);
   const [txt,setTxt]=useState("");
 
   return(
@@ -14,9 +15,8 @@ const App=()=>{
      <h1>Wellcome Theme Change Program</h1>
      <div style={{marginLeft:"150px"}}>
      <input type="text" placeholder="Enter Color"  value={txt} onChange={(e)=>{setTxt(e.target.value)}} />
-     <button>Change Color</button> <br /> <br />
-     <div style={{width:"300px",height:"300px",border:"2px solid black",backgroundColor:"blue"}}></div>
-     </div>
+     <button onClick={()=>{setColor(txt)}}>Change Color</button> <br /> <br />
+     <div style={{width:"300px",height:"300px",border:"2px solid black",backgroundColor:color}}></div></div>
      </Container>
      </>
   )
--- Theme Changer By Context API/src/main.jsx
+++ Theme Changer By Context API/src/main.jsx
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// import Store from './Store.jsx';
+// import { Provider } from 'react-redux';
+import ColorContext from './ColorContext.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
-   
+    <ColorContext>
      <App /> 
+    </ColorContext>
+       
+
                
 )
